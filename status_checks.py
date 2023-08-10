d6b9655f6ebd8839c378d8370f8c1866073a0fb7
Daniel Löfquist <daniel.lofquist@hiq.se>
2023-08-10 17:27:12 +0200

    feat: add home server service check (jellyfin)

--- status_checks.py
+++ status_checks.py
@@ -18,6 +18,7 @@ class ServiceStatus:
 def show_internet_connection(status_zero: StatusZero, input_queue: queue.Queue):
     while True:
         msg = input_queue.get()
+        # print(msg)
         if msg == ServiceStatus.UP:
             status_zero.one.green.on()
             status_zero.one.red.off()
@@ -53,15 +54,64 @@ def check_internet_connection(output_queue):
             output_queue.put(ServiceStatus.DOWN)
         sleep(10)
 
+def show_home_server(status_zero: StatusZero, input_queue: queue.Queue):
+    while True:
+        msg = input_queue.get()
+        # print(msg)
+        if msg == ServiceStatus.UP:
+            status_zero.two.green.on()
+            status_zero.two.red.off()
+        elif msg == ServiceStatus.DOWN:
+            status_zero.two.green.off()
+            status_zero.two.red.on()
+        else:
+            status_zero.two.green.on()
+            status_zero.two.red.on()
+
+def check_jellyfin():
+    jellyfin_health_url = 'http://my-jellyfin:8096/health'
+    try:
+        response = requests.get(jellyfin_health_url, allow_redirects=True, timeout=3)
+        if response.status_code == 200 and response.text == 'Healthy':
+            return True
+        else:
+            return False
+    except Timeout:
+        return False
+
+
+def check_home_server(output_queue):
+    while True:
+        checks = []
+        checks.append(check_jellyfin())
+        all_checks = all(item == True for item in checks)
+        if all_checks:
+            output_queue.put(ServiceStatus.UP)
+        else:
+            output_queue.put(ServiceStatus.DOWN)
+        sleep(10)
+
 
 status_zero = StatusZero()
+
 check_internet_queue = queue.Queue()
+check_home_server_queue = queue.Queue()
+#check_home_assistant_queue = queue.Queue()
 
 thread1 = threading.Thread(target=show_internet_connection, args=(status_zero,check_internet_queue,))
 thread2 = threading.Thread(target=check_internet_connection, args=(check_internet_queue,))
 
+thread3 = threading.Thread(target=show_home_server, args=(status_zero,check_home_server_queue,))
+thread4 = threading.Thread(target=check_home_server, args=(check_home_server_queue,))
+
+#thread5 = threading.Thread(target=show_home_assistant, args=(status_zero,check_home_assistant_queue,))
+#thread6 = threading.Thread(target=check_home_assistant, args=(check_home_assistant_queue,))
+
 thread1.start()
 thread2.start()
+
+thread3.start()
+thread4.start()
 
 while True:
     pass
